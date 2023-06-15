95ad85b9e97ccb4484c333220e7bb5fb9460e3e5
신성윤 <60770212+since1630@users.noreply.github.com>
2023-06-15 19:58:12 +0900

    [level 2] Title: 구명보트, Time: 11.26 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,9 +1,11 @@
 function solution(people, limit) {
-    let cnt = 0;
-    people.sort((a,b) => (b-a));
+    let cnt = 0
     let left = 0;
     let right = people.length - 1;
-
+    people.sort((a,b) => (b-a))
+    
+    [80,50,40]
+    
     while (left <= right) {
       if (people[left] + people[right] <= limit) {
         left++;
@@ -13,5 +15,5 @@ function solution(people, limit) {
       }
       cnt++;
     }
-    return cnt;
+    return cnt
 }
