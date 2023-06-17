c046239cf5b45a955b285c6cdee09afef43cff2a
신성윤 <60770212+since1630@users.noreply.github.com>
2023-06-17 11:31:08 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -2,10 +2,8 @@ function solution(x) {
   let copy_x = x;
   let jarisuSum = 0;
   while (copy_x > 0) {
-    let remain = copy_x % 10;
-    jarisuSum += remain;
+    jarisuSum += copy_x % 10;
     copy_x = Math.floor(copy_x / 10);
   }
-  console.log(jarisuSum);
   return x % jarisuSum === 0 ? true : false;
 }
