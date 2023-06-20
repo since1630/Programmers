2c9960d5b973f6dae609b0afff6d880e26593e03
신성윤 <60770212+since1630@users.noreply.github.com>
2023-06-20 16:04:34 +0900

    [level 1] Title: 시저 암호, Time: 1.79 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,38 @@
+function solution(s, n) {
+  let str = "";
+  const arr = s.split("");
+  arr.forEach((a) => {
+    let word = a.charCodeAt(0);
+    // console.log(word);
+      
+    // 1.word가 공백을 포함한 기타 문자인 경우
+    if (
+      word === 32 ||
+      (word > 32 && word <= 64) ||
+      (word >= 91 && word <= 96)
+    ) {
+      str += String.fromCharCode(word);
+    }
+    // 2.word가 대문자인 경우
+    else if (word >= 65 && word <= 90) {
+      word += n;
+      if (word > 90) {
+        word = word - 26;
+      }
+      str += String.fromCharCode(word);
+    }
+    // 3.word가 소문자인 경우
+    else if (word >= 97 && word <= 122) {
+      word += n;
+      if (word > 122) {
+        word = word - 26;
+      }
+      str += String.fromCharCode(word);
+    }
+  });
+
+  if (str.indexOf("!")) {
+    str = str.replace(/!/gm, " ");
+  }
+  return str;
+}
