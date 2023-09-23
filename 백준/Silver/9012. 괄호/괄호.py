f4fc07baeb1fefc8568f93f9cd63cad48e575635
신성윤 <60770212+since1630@users.noreply.github.com>
2023-09-24 00:16:39 +0900

    [Silver IV] Title: 괄호, Time: 144 ms, Memory: 115644 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,20 +1,15 @@
 n = int(input())
 
 for _ in range(n):
-    lst = []
-    cmd = input()
-    for word in cmd :
-        if word == '(':
-            lst.append(word)
-        elif word == ')':
-            if lst:
-                lst.pop()
-            else:
-                lst.append(word)
-                # print('NO')
-                break
-    # print(lst)
-    if lst:
-        print('NO')
+    stack = []
+    str = input()
+    for word in str:
+        if (len(stack) != 0 and word == ")"):
+            if (word not in stack):
+                stack.pop()
+        else:
+            stack.append(word)
+    if (len(stack) == 0):
+        print("YES")
     else:
-        print('YES')
+        print("NO")
