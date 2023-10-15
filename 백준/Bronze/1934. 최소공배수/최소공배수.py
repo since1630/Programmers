1a1d5e0fd37517387b92304c7219dc0da6ded420
신성윤 <60770212+since1630@users.noreply.github.com>
2023-10-15 12:41:50 +0900

    [Bronze I] Title: 최소공배수, Time: 132 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,15 @@
+
+n = int(input())
+
+def gcd(a,b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b,a%b)
+    
+
+for _ in range(n):
+    a, b = map(int,input().split(' '))
+    g = gcd(a,b)
+    print((a*b) // g)
+
